163ae3b3cc36a266978858babdb6a622dda06775
hyperxpizza <wojtek.frackowski@gmail.com>
2021-09-01 19:48:04 +0200

    add footer component

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import "./footer.css";
 //bootstrap components
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 const Footer = () => {
     return (
-        <Container>
-
-        </Container>
+        <div id="footer">
+            <Container className="pt-5">
+                <Row>
+                    <Col>
+                        <h4>Footer</h4>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
 
