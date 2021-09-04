ac5e208e6ff44781d927502459950b5b1622fc58
hyperxpizza <wojtek.frackowski@gmail.com>
2021-09-04 14:10:44 +0200

    logout action

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -66,3 +66,11 @@ export const login = (username, password) => (dispatch) => {
         }
     );
 };
+
+export const logout = () => (dispatch) => {
+    AuthService.logout();
+
+    dispatch({
+        type: LOGOUT
+    });
+};
